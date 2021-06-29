2962b790f4052272f644e0d230c7744f84ce78c0
Juel07 <juel.braganza97@gmail.com>
2021-06-29 15:32:53 +0100

    add new_space form + post route tix-#6

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require "sinatra/base"
 require "sinatra/reloader"
 require "sinatra/flash"
 require "./database_connection_setup"
+require_relative "lib/space"
 require "uri"
-
 
 class MakersBnb < Sinatra::Base
   configure :development do
@@ -17,16 +17,21 @@ class MakersBnb < Sinatra::Base
     "Hello JAMI"
   end
 
-  get '/' do
-    redirect('/spaces')
+  get "/" do
+    redirect("/spaces")
   end
 
-  get '/spaces' do
+  get "/spaces" do
     erb(:spaces)
   end
 
-  get '/spaces/new' do
+  get "/spaces/new" do
     erb :new_space
+  end
+
+  post "/spaces/add" do
+    Space.add(params[:name], params[:description], params[:city], params[:price], params[:hero_image])
+    redirect "/spaces"
   end
 
   run! if app_file == $0
