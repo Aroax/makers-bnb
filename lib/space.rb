11f81b8e296a4c8dde44f3d3aa81dfe9e7d7cb99
IrinaGC <irina.g.cirlan@gmail.com>
2021-06-29 16:52:00 +0100

    premerge check point

--- lib/space.rb
+++ lib/space.rb
@@ -20,4 +20,15 @@ class Space
     end
   end
 
+  def self.add(name:,description:,city:,price:,hero_image:)
+    result = DatabaseConnection.query(sql:"INSERT INTO space (name, description, city, price, hero_image) VALUES ($1, $2, $3, $4, $5) RETURNING name, description, city, price, hero_image;", params: [name, description, city, price, hero_image])
+    space = Space.new(
+      name: result[0]['name'],
+      description: result[0]['description'],
+      city: result[0]['city'],
+      price: result[0]['price'],
+      hero_image: result[0]['hero_image']
+    )
+  end
+
 end
--- spec/units/space_spec.rb
+++ spec/units/space_spec.rb
@@ -2,11 +2,12 @@ require_relative '../../lib/space'
 require_relative '../setup_test_database'
 
 describe Space do
+  
   describe '.all' do
     it 'displays all spaces' do
       add_row_to_test_space_table
       space = Space.all
-
+      
       expect(space.length).to eq 1
       expect(space.first).to be_a Space
       # expect(space.first.id).to eq space.id
@@ -15,6 +16,23 @@ describe Space do
       expect(space.first.price).to eq "400"
       expect(space.first.city).to eq "london"
       expect(space.first.hero_image).to eq "./public/images/placeholder.jpg"
+    end
+  end
+  
+  let(:name) { double(:name) }
+  let(:description) { double(:description) }
+  let(:price) { 350 }
+  let(:city) { double(:city) }
+  let(:hero_image) { double(:hero_image) }
+
+  describe '.add' do
+    it 'adds a new space to the database' do
+      space = Space.add(name: name, description: description, city: city, price: price, hero_image: hero_image)
+
+      expect(space.name).to eq "#{name}"
+      expect(space.description).to eq "#{description}"
+      expect(space.city).to eq "#{city}"
+      expect(space.price).to eq "#{price}"
     end
   end
 end
