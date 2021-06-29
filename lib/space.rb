688fb658ba89b23b2c0b46fc9cc38c2dff6f7086
Arran Oakes <arran.oakes@gmail.com>
2021-06-29 13:52:30 +0100

    database connection debugging

--- lib/space.rb
+++ lib/space.rb
@@ -1,3 +1,26 @@
+require_relative 'database_connection'
+
 class Space
+  attr_reader :name, :description, :city, :price, :hero_image
+
+  def self.add(name:,description:,city:,price:,hero_image:)
+    result = DatabaseConnection.query(sql:"INSERT INTO space (name, description, city, price, hero_image) VALUES ($1, $2, $3, $4, $5) RETURNING (name, description, city, price, hero_image);", params: [name, description, city, price, hero_image])
+    p result
+    # Space.new(
+    #   name: result[0]['name'],
+    #   description: result[0]['description'],
+    #   city: result[0]['city'],
+    #   price: result[0]['price'],
+    #   hero_image: result[0]['hero_image']
+    # )
+  end
+
+  def initialize(name:, description:, city:, price:, hero_image:)
+    @name = name
+    @description = description
+    @city = city
+    @price = price
+    @hero_image = hero_image
+  end
 
 end
--- spec/units/space_spec.rb
+++ spec/units/space_spec.rb
@@ -1,8 +1,26 @@
 require_relative '../../lib/space'
 
 describe Space do
-  
+  let(:name) { double(:name) }
+  let(:description) { double(:description) }
+  let(:price) { 350 }
+  let(:city) { double(:city) }
+  let(:hero_image) { double(:hero_image) }
+
+
   it 'is an infinite void' do
+  end
+
+  describe '.add' do
+    it 'adds a new space to the database' do
+      space = Space.add(name: name, description: description, city: city, price: price, hero_image: hero_image)
+
+      expect(space.name).to eq name
+      expect(space.description).to eq description
+      expect(space.city).to eq city
+      expect(space.price).to eq price
+    end
+
   end
 
 end
