53ff12a830af43083ef0c1cd148cc692165ee42c
Juel07 <juel.braganza97@gmail.com>
2021-06-29 17:48:34 +0100

    error message dump for @aroax

--- lib/space.rb
+++ lib/space.rb
@@ -1,27 +1,39 @@
-require_relative 'database_connection'
+require_relative "database_connection"
 
 class Space
-  attr_reader :name, :description, :city, :price, :hero_image
+  attr_reader :id, :name, :description, :city, :price, :hero_image
 
-  def self.add(name:,description:,city:,price:,hero_image:)
-    result = DatabaseConnection.query(sql:"INSERT INTO space (name, description, city, price, hero_image) VALUES ($1, $2, $3, $4, $5) RETURNING name, description, city, price, hero_image;", params: [name, description, city, price, hero_image])
-    # p result
+  def self.add(name:, description:, city:, price:, hero_image:)
+    result = DatabaseConnection.query(sql: "INSERT INTO space (name, description, city, price, hero_image) VALUES ($1, $2, $3, $4, $5) RETURNING id, name, description, city, price, hero_image;", params: [name, description, city, price, hero_image])
     space = Space.new(
-      name: result[0]['name'],
-      description: result[0]['description'],
-      city: result[0]['city'],
-      price: result[0]['price'],
-      hero_image: result[0]['hero_image']
+      id: result[0]["id"],
+      name: result[0]["name"],
+      description: result[0]["description"],
+      city: result[0]["city"],
+      price: result[0]["price"],
+      hero_image: result[0]["hero_image"],
     )
-    # p space
   end
 
-  def initialize(name:, description:, city:, price:, hero_image:)
+  def self.find_by_id(space_id:)
+    result = DatabaseConnection.query(sql: "SELECT * FROM space WHERE id = $1;", params: [space_id])
+
+    Space.new(
+      id: result[0]["id"],
+      name: result[0]["name"],
+      description: result[0]["description"],
+      city: result[0]["city"],
+      price: result[0]["price"],
+      hero_image: result[0]["hero_image"],
+    )
+  end
+
+  def initialize(id:, name:, description:, city:, price:, hero_image:)
+    @id = id
     @name = name
     @description = description
     @city = city
     @price = price
     @hero_image = hero_image
   end
-
 end
