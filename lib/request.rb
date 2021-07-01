9db7141681e7569aa1a9a8d0030d7b1bea0853d7
Arran Oakes <arran.oakes@gmail.com>
2021-07-01 19:29:23 +0100

    Request SQL query seems to work in tableplus but only returns the first row to the Object

--- lib/request.rb
+++ lib/request.rb
@@ -4,23 +4,25 @@ class Request
   attr_reader :customer_id, :space_id, :space_name, :date_in, :date_out, :hero_image, :request_status
 
   def self.dashboard
-    result = DatabaseConnection.query(sql: "SELECT customer.id, booking.space_id, space.name, booking.date_in, booking.date_out, space.hero_image, booking.request FROM customer JOIN booking ON customer.id=booking.customer_id JOIN space ON booking.space_id=space.id;", params: [])
+    result = DatabaseConnection.query(sql: "SELECT * FROM customer INNER JOIN booking ON customer.id=booking.customer_id INNER JOIN space ON booking.space_id=space.id;", params: [])
     p result
+    # customer.id, booking.space_id, space.name, booking.date_in, booking.date_out, space.hero_image, booking.request
     # DatabaseConnection.query(sql: "SELECT customer.id, booking.space_id, space.name, booking.date_in, booking.date_out, space.hero_image, booking.request FROM customer JOIN booking ON customer.id=booking.customer_id JOIN space ON booking.space_id=space.id;", params: [])
     result.map do |request|
       Request.new(
-        customer_id: result[0]['id'],
-        space_id: result[0]['booking.space_id'],
-        space_name: result[0]['space.name'],
-        date_in: result[0]['booking.date_in'],
-        date_out: result[0]['booking.date_out'],
-        hero_image: result[0]['space.hero_image'],
-        request_status: result[0]['booking.request']
+        customer_id: result[0]["id"],
+        space_id: result[0]["space_id"],
+        price: result[0]["price"],
+        space_name: result[0]["name"],
+        date_in: result[0]["date_in"],
+        date_out: result[0]["date_out"],
+        hero_image: result[0]["hero_image"],
+        request_status: result[0]["request"]
       )
     end
   end
 
-  def initialize(customer_id:,space_id:,space_name:,date_in:,date_out:,hero_image:,request_status:)
+  def initialize(customer_id:,space_id:,space_name:,date_in:,date_out:,hero_image:,request_status:, price:)
     @customer_id = customer_id
     @space_id = space_id
     @space_name = space_name
@@ -28,6 +30,7 @@ class Request
     @date_out = date_out
     @hero_image = hero_image
     @request_status = request_status
+    @price = price
   end
 
 end
